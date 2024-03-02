86ebc3d91f7cbe437e64028cba708b4efdf07669
catinrage <catinrage@gmail.com>
2024-03-02 15:43:41 +0330

    Add import and use add function, update server response

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { add } from "./lib/functions";
+
+const result = add(1, 2);
+
 const server = Bun.serve({
   port: 3000,
   fetch(request) {
-    return new Response("Welcome to Bun!");
+    return new Response(`Hello, world! ${result}`);
   },
 });
 
